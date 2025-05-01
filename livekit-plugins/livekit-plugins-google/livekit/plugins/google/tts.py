2464d9a472e4ffbe6d0f700ec13bcb87bad85736
Pavel Shakhmin <43382033+pshakhmin@users.noreply.github.com>
2025-05-01 17:42:18 +0300

    fix: encoding name

--- livekit-plugins/livekit-plugins-google/livekit/plugins/google/tts.py
+++ livekit-plugins/livekit-plugins-google/livekit/plugins/google/tts.py
@@ -105,7 +105,7 @@ class TTS(tts.TTS):
         self._opts = _TTSOptions(
             voice=voice_params,
             audio_config=texttospeech.AudioConfig(
-                audio_encoding=texttospeech.AudioEncoding.PCM,
+                audio_encoding=texttospeech.AudioEncoding.LINEAR16,
                 sample_rate_hertz=sample_rate,
                 pitch=pitch,
                 effects_profile_id=effects_profile_id,
